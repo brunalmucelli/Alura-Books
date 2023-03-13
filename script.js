bd6a3ce11ac738f956485d35d9b6cf7149f088d1
brunalmucelli <brunalmucelli@gmail.com>
2023-03-13 18:51:41 -0300

    Uso promisse all

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-var consultaCEP= fetch('https://viacep.com.br/ws/01001000/json/');
-console.log(consultaCEP);
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
+let ceps = ['31748193', '01001001'];
+
+let conjuntoCeps = ceps.map(valor => buscaEndereco(valor));
+//console.log(conjuntoCeps);
+
+Promise.all(conjuntoCeps).then(resposta => console.log(resposta));
+
+
+
+
+
+
+
+
+/*.then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse cep não existe!');
+        } else {
+            console.log(r)
+        }
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem=>console.log('Processamento concluido'));
+
+console.log(consultaCEP);*/
